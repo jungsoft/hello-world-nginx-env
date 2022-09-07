2726ee0867596c9a9058a0d2f7c3fdbefaaa606e
Rafael Jung <jungrafael@jungsoft.io>
2022-09-07 18:46:20 +0200

    add multiple message ENVs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 set -eu
 
-sed -i "s/message_env/${MESSAGE:--}/g" "/usr/share/nginx/html/index.html"
+sed -i "s/message_env1/${MESSAGE1:--}/g" "/usr/share/nginx/html/index.html"
+sed -i "s/message_env2/${MESSAGE2:--}/g" "/usr/share/nginx/html/index.html"
+sed -i "s/message_env3/${MESSAGE3:--}/g" "/usr/share/nginx/html/index.html"
+sed -i "s/message_env4/${MESSAGE4:--}/g" "/usr/share/nginx/html/index.html"
 
 exec "$@"
